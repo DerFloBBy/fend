3a3330d9be17dfc6454305eb8bb9793bb5e7eca0
FBOELKE <fboelke@emea.corpdir.net>
2022-03-29 11:31:10 +0200

    Rebuild Nav-Menu

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -46,15 +46,28 @@ const navlist = document.getElementById("navbar__list");            // Nav__List
 
 // build the nav
 
+/* This is easier but NOT ALLOWED! */
+/*
 function buildNav(){                                           // Build with Attributes of all <section>'s the NAV
     for (let section of sections) {
         // console.log(section.getAttribute("data-nav"));
         const section_data = section.getAttribute("data-nav");
-        navlist.insertAdjacentHTML('beforeend', `<li class="menu__link">${section_data}</li>`);
+        const section_id = section.getAttribute("id");
+        navlist.insertAdjacentHTML('beforeend', `<li><a href="#${section_id}" class="menu__link">${section_data}</a></li>`);
     }
 }
+*/
 
-buildNav();
+function buildNav(){                                         // Build with Attributes of all <section>'s the NAV
+    for (const section of sections) {
+        newListItem = document.createElement('li');
+        newListItemLink = document.createElement('a');
+        newListItemLink.textContent = section.dataset.nav;
+        newListItemLink.classList.add('menu__link');
+        newListItem.appendChild(newListItemLink);
+        navlist.appendChild(newListItem);
+    }
+}
 
 // Add class 'active' to section when near top of viewport
 
@@ -69,6 +82,7 @@ buildNav();
 */
 
 // Build menu 
+buildNav();
 
 // Scroll to section on link click
 
