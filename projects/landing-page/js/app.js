2de2f1069da33484d380bd2fe49edbd80237a2d6
FBOELKE <fboelke@emea.corpdir.net>
2022-03-31 11:50:02 +0200

    Comments revised

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -22,64 +22,71 @@
  * Define Global Variables
  * 
 */
-// const sections = document.getElementsByTagName("section");          // creates a HTML Collection of all <section>'s
-const sections = document.querySelectorAll("section");          // creates a HTML Collection of all <section>'s
-const navlist = document.getElementById("navbar__list");            // Nav__List
 
+const sections = document.querySelectorAll("section");              // creates a NodeList of all <section>'s
+const navlist = document.getElementById("navbar__list");            // Nav__List
 
 /**
  * End Global Variables
- * Start Helper Functions
  * 
+ * Start Helper Functions
 */
-// function getSections(){                                           // get Attributes of all <section>'s
-//     for (let section of sections) {
-//         console.log(section.getAttribute("data-nav"));
-//     }
-// }
 
 
 /**
  * End Helper Functions
- * Begin Main Functions
  * 
+ * Begin Main Functions
 */
 
-// build the nav
+// Build the nav
+// *************
 
-/* This is easier but NOT ALLOWED! */
+/* The following is easier but NOT ALLOWED according to the rubric! */
 /*
-function buildNav(){                                           // Build with Attributes of all <section>'s the NAV
+// Build the NAV with Attributes of all <section>'s
+function buildNav(){
     for (let section of sections) {
-        // console.log(section.getAttribute("data-nav"));
+        // section.Attributes for ListItem.innerText and ListItem.href
         const section_data = section.getAttribute("data-nav");
         const section_id = section.getAttribute("id");
+
+        // put ListItem with Anchor at the end of unorderd List
         navlist.insertAdjacentHTML('beforeend', `<li><a href="#${section_id}" class="menu__link">${section_data}</a></li>`);
     }
 }
 */
 
-function buildNav(){                                         // Build with Attributes of all <section>'s the NAV
+/* The following is according to the rubric! */
+// Build the NAV with Attributes of all <section>'s
+function buildNav(){
     for (const section of sections) {
+        // create 'li' and 'a' Element
         newListItem = document.createElement('li');
         newListItemLink = document.createElement('a');
+        // section.dataset.nav holds the Section-No (equal to the header <h2>)
         newListItemLink.textContent = section.dataset.nav;
+        // add class, data-nav (used for scrollToSection) and id (used for highlighting nav-section when aktiv) too the 'a' Element
         newListItemLink.classList.add('menu__link');
         newListItemLink.setAttribute('data-nav', section.id);
         newListItemLink.id = `nav_${section.id}`;
+
         newListItem.appendChild(newListItemLink);
         navlist.appendChild(newListItem);
     }
 }
 
 // Add class 'active' to section when near top of viewport
+// *******************************************************
 function activeSection() {
     for (const section of sections) {
+        // getBoundingClientRect() to get the position of each section
         let rect = section.getBoundingClientRect();
         let top = rect.top;
         let bottom = rect.bottom;
 
         section.classList.remove('your-active-class');
+        // if section is in view, it gets the active-class (Nav-Section is highlighted too)
         if ((0 <= top) && (top < bottom-top)) {
             section.classList.add('your-active-class');
             document.querySelector(`#nav_${section.id}`).classList.add('menu__link-active');
@@ -90,16 +97,21 @@ function activeSection() {
 }
 
 
-// Scroll to anchor ID using scrollTO event
+// Scroll to anchor ID using scrollIntoView event
+// **********************************************
 function scrollToSection(evt){
     document.getElementById(evt.target.getAttribute("data-nav")).scrollIntoView();
 }
 
 // Hide the Navbar if downscroll
+// *****************************
 function hideNavbar(){
-    if (window.pageYOffset > scrollPosition){                               // scrollPosition: initialized to pageYOffset
+    // scrollPosition: initialized to pageYOffset
+    if (window.pageYOffset > scrollPosition){
+        // scroll DOWN / hide Navbar
         document.querySelector('.navbar__menu').style.display = "none";
     } else {
+        // scroll UP / display Navbar
         document.querySelector('.navbar__menu').style.display = "block";
     }
     scrollPosition = window.pageYOffset;
@@ -107,20 +119,24 @@ function hideNavbar(){
 
 /**
  * End Main Functions
- * Begin Events
  * 
+ * Begin Events
 */
 
-// Build menu 
+// Build Navbar
+// ************
 buildNav();
 
 // Scroll to section on link click
+// *******************************
 navlist.addEventListener('click', scrollToSection);
 
 // Set sections as active
+// **********************
 document.addEventListener('scroll', activeSection);
 activeSection();
 
-// Hide Navbar if downscroll
+// Hide Navbar if scroll down
+// **************************
 document.addEventListener('scroll', hideNavbar);
 let scrollPosition = window.pageYOffset;
